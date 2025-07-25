eb38cd58d41b33041ce9a316ae48fddc98d67959
Sneferu777 <haithamderrazi@gmail.com>
2025-07-25 11:14:47 +0100

    Update Reversing a string using Stack in C++.cpp

--- Reversing a string using Stack in C++.cpp
+++ Reversing a string using Stack in C++.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 #include<stack>
 
+// C implementation we must include <string.h> <stdio.h>
+void reverseSTR(char* str){
+    char stack[100]; int top = -1;
+    size_t len = strlen(str);
+    for(int i = 0; i< len; i++)
+        stack[++top] = str[i];
+    
+    for(int i = 0; i < len; i++){
+        str[i] = stack[top--];
+    }
+    puts(str);
+}
+
+
 int main(){
   
     std::string str = "Sneferu";
@@ -14,6 +28,9 @@ int main(){
         s.pop();
     }
     std::cout<<"Reversed\t" << str <<std::endl;
+
     
+    
+
 
 return 0;}
